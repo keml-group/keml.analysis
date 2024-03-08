a37d5af5545ac11f7dfff26ca7ada07df4d7b350
Susanne Göbel <susanne.goebel@buildup.ch>
2024-03-08 15:50:50 +0100

    Changed to maven porject, added kotlin dependency

--- src/keml/analysis/AnalysisProvider.java
+++ src/keml/analysis/AnalysisProvider.java
@@ -1,11 +1,19 @@
 package keml.analysis;
 
+import org.apache.commons.io.FilenameUtils;
+
+import keml.Conversation;
+
+import keml.io.KemlFileHandler;
+
 public class AnalysisProvider {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		String source = "../../graphs/objective3-2-2v4.keml";
+		String target = FilenameUtils.removeExtension(source) + ".csv";
 
+		Conversation conv = new KemlFileHandler().loadKeml(source);
+		NodeAnalyser.Companion.createCSV(conv, target);
 	}
 
 }
---
+++ src/keml/analysis/NodeAnalyser.kt
@@ -0,0 +1,29 @@
+package keml.analysis
+
+import keml.Conversation
+import org.apache.commons.csv.CSVFormat;
+import java.io.File
+
+fun createCSV(conv: Conversation, path: String) {
+		//File(path).bufferedWriter().use { out ->
+ 
+		//CSVFormat.DEFAULT.print(out).apply {}
+	
+		//}
+
+		}
+
+class NodeAnalyser {
+	
+	companion object {
+		fun createCSV(conv: Conversation, path: String) {
+		//File(path).bufferedWriter().use { out ->
+ 
+		//CSVFormat.DEFAULT.print(out).apply {}
+	
+		//}
+
+		}
+	}
+
+}
