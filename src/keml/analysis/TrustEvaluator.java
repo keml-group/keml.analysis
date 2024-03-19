59cbf57a6a8674c3285f8201927966b7f2b01ec2
Susanne Göbel <susanne.goebel@buildup.ch>
2024-03-19 19:09:18 +0100

    Trust analyser with endless loop detection, these problems should be managed on io modul or core

--- src/keml/analysis/TrustEvaluator.java
+++ src/keml/analysis/TrustEvaluator.java
@@ -4,19 +4,26 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.io.FilenameUtils;
 
 import keml.Conversation;
+import keml.Information;
+import keml.InformationLink;
 import keml.NewInformation;
 import keml.PreKnowledge;
 import keml.ReceiveMessage;
-import keml.analysis.ConversationAnalyser.InformationType;
 
 public class TrustEvaluator {
 	// +1 is absolute trust, -1 is absolute distrust, 0 is ignore - no knowledge about that node
@@ -26,6 +33,7 @@ public class TrustEvaluator {
 	List<ReceiveMessage> receives;  //starting point for work on knowledge part
 	List<NewInformation> newInfos;
 	List<PreKnowledge> preKnowledge;
+	
 	
 	public TrustEvaluator(Conversation conv) {
 		this.conv = conv;
@@ -69,7 +77,83 @@ public class TrustEvaluator {
 	}
 	
 	public void evaluate() {
-		// TODO
+		HashSet<Information> toVisit = Stream.concat(preKnowledge.stream(), newInfos.stream()).collect(Collectors.toCollection(HashSet::new));
+		//List<Information> toVisit = Stream.concat(preKnowledge.stream(), newInfos.stream()).toList().reversed();// start with last time stamp
+		
+		while (toVisit.size()>0) {
+			int remaining = toVisit.size();
+			for (Iterator<Information> i = toVisit.iterator(); i.hasNext(); ) {
+				 Information info = i.next();
+				 if (ready(info, toVisit)) {
+					 info.setCurrentTrust(currentNodeTrust(info));
+					 i.remove();
+				 }
+			}
+			if (toVisit.size() == remaining) {
+				System.err.println(toVisit.toString());
+				for (Information i: toVisit) {
+					for(InformationLink l : i.getTargetedBy()) {
+						System.err.println(l.getSource().getMessage() + " -> "+i.getMessage());
+					}
+				}
+				throw new IllegalArgumentException("Endless loop of "+ toVisit.size() +" nodes - please check the argumentation graph");
+			}
+		}
+	}
+	
+	private boolean ready(Information info, HashSet<Information> toVisit) {
+		for (InformationLink informationLink : info.getTargetedBy()) {
+			if (toVisit.contains(informationLink.getSource()))
+				return false;
+		}
+		return true;
+	}
+	
+	private Float currentNodeTrust(Information info) {
+		return limitTo1(info.getInitialTrust()
+			+ getRepetitionScore(info)
+			+ 2*argumentationScore(info));
+	}
+	
+	// relies on current trust 
+	private float argumentationScore(Information info) {
+		return (float) info.getTargetedBy().stream()
+			.mapToDouble(link -> score(link))
+			.sum();
+	}
+	
+	private float score(InformationLink link) {
+		float edgeWeight;
+		switch(link.getType()) {
+		case SUPPORT:
+			edgeWeight = 0.5f;
+			break;
+		case ACCEPT:
+			edgeWeight = 1.0f;
+			break;
+		case CHALLENGE:
+			edgeWeight = -0.5f;
+			break;
+		case REJECT:
+			edgeWeight = -1.0f;
+			break;
+		case SUPPLEMENT:
+		default:
+			edgeWeight = 0.0f;
+			break;	
+		}
+		// TODO should we ignore nodes that have negative trust or let them work in opposite direction? Currently opposite:
+		return edgeWeight*link.getSource().getCurrentTrust();
+	}
+	
+	private float getRepetitionScore(Information info) {
+		return (float) info.getRepeatedBy().size()/receives.size();
+	}
+
+	private static Float limitTo1(Float f) {
+		if (f<-1) return -1F;
+		if (f>1) return 1F;
+		return f;
 	}
 	
 	// write current values
