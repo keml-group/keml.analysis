2d3c5c5f0e97ee34b72152e03bc35995399f1bd4
Noah Heuser <nheuser@uni-koblenz.de>
2025-03-31 20:02:28 +0200

    simple server that receives keml-json and sends back a zip-archive of the analysis files

--- src/keml/analysis/AnalysisProvider.java
+++ src/keml/analysis/AnalysisProvider.java
@@ -4,54 +4,63 @@ import java.io.File;
 import java.util.Locale;
 
 import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.util.LocaleUtil;
 
 import keml.Conversation;
 import keml.io.KemlFileHandler;
 
-import org.apache.poi.util.LocaleUtil;
-
- 
-
 public class AnalysisProvider {
 
+	public static void runAnalysis(File json) throws Exception {
+		String source = json.getAbsolutePath();
+		Conversation conv = new KemlFileHandler().loadKemlJSON(source);
+		String basePath = "../keml.sample/introductoryExamples/analysis/test//"
+				+ FilenameUtils.removeExtension(json.getName());
+		new ConversationAnalyser(conv).createCSVs(basePath);
+		LocaleUtil.setUserLocale(Locale.US);
+		for (int i = 2; i <= 10; i++) {
+			TrustEvaluator trusty = new TrustEvaluator(conv, i);
+			trusty.writeRowAnalysis(basePath + "-w" + i + "-",
+					TrustEvaluator.standardTrustConfigurations(conv.getConversationPartners()), 1.0F);
+		}
+	}
+
 	public static void main(String[] args) throws Exception {
-		
+
 		String folder;
 		if (args.length == 0) {
 			folder = "../keml.sample/introductoryExamples";
 		} else {
-			folder = args[0];			
+			folder = args[0];
 		}
-		
+
 		File sourceFolder = new File(folder + "/keml/");
-		File targetFolder = new File(folder+ "/analysis/");
+		File targetFolder = new File(folder + "/analysis/");
 
 		// if directory contains .keml but no ../analysis/
-		if (sourceFolder.exists() && !targetFolder.exists())
+		if (sourceFolder.exists() && !targetFolder.exists()) {
 			targetFolder.mkdirs();
+		}
 
-		System.out.println("You started the KEML analysis.\n I will read KEML files from " + folder 
+		System.out.println("You started the KEML analysis.\n I will read KEML files from " + folder
 				+ ".\n I will write the resulting files into " + targetFolder);
 
-		
 		File[] files = sourceFolder.listFiles((dir, name) -> name.toLowerCase().endsWith(".keml"));
-		
-		for (File file: files) {
+
+		for (File file : files) {
 			try {
 				String source = file.getAbsolutePath();
 				Conversation conv = new KemlFileHandler().loadKeml(source);
-				
-				String basePath = targetFolder +"/" + FilenameUtils.removeExtension(file.getName());
+
+				String basePath = targetFolder + "/" + FilenameUtils.removeExtension(file.getName());
 
 				new ConversationAnalyser(conv).createCSVs(basePath);
 				LocaleUtil.setUserLocale(Locale.US);
-				
-				for(int i = 2; i<= 10; i++) {
+
+				for (int i = 2; i <= 10; i++) {
 					TrustEvaluator trusty = new TrustEvaluator(conv, i);
-					trusty.writeRowAnalysis(
-							basePath+"-w"+i+"-",
-							TrustEvaluator.standardTrustConfigurations(conv.getConversationPartners()),
-							1.0F);
+					trusty.writeRowAnalysis(basePath + "-w" + i + "-",
+							TrustEvaluator.standardTrustConfigurations(conv.getConversationPartners()), 1.0F);
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
